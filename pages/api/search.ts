0e2a8539e69890ff6458202343a070a9fbf34666
Katelin Frederick <k.frederick94@gmail.com>
2022-12-07 17:20:49 -0600

    Add no results found content

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -7,6 +7,10 @@ export default async function handler(req, res) {
     const response = await fetch(`https://www.omdbapi.com/?page=${page}&s=${searchTerm}&y=${year}&type=${searchType}&apikey=${process.env.OMDBI_KEY}`)
       .then(response => response.json())
       .then(data => {
+        if (data.Error === 'Movie not found!') {
+          return { results: [], totalResults: 0 }
+        }
+
         return { results: data.Search, totalResults: data.totalResults }
       })
 
--- project/search/search.tsx
+++ project/search/search.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import axios from 'axios'
 import { Formik, Form } from 'formik'
-import validationSchema from './validation/validationSchema'
+import Pagination from '../../components/Pagination'
+import Spinner from '../../components/Spinner'
 import SearchForm from './structure/SearchForm'
 import MovieList from './structure/MovieList/MovieList'
-import Pagination from '../../components/Pagination'
+import validationSchema from './validation/validationSchema'
 
 const Search = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [results, setResults] = useState([])
+  const [showNoResults, setShowNoResults] = useState(false)
   const [totalPages, setTotalPages] = useState(0)
 
   const initialValues = {
@@ -17,6 +20,8 @@ const Search = () => {
   }
 
   const onSubmit = async (values, page) => {
+    setIsLoading(true)
+
     const { searchTerm, searchType, year } = values
 
     try {
@@ -29,6 +34,8 @@ const Search = () => {
         },
       })
         .then(function (response) {
+          // console.log('results', response.data.results.filter((item) => item.Type !== 'movie' && item.Type !== 'series' && item.Type !== 'game'))
+          setShowNoResults(response.data.results.length === 0)
           setResults(response.data.results)
           setTotalPages(Math.ceil(response.data.totalResults / 10))
         })
@@ -38,10 +45,12 @@ const Search = () => {
     } catch (error) {
       console.error('ERROR Fetching Data', error)
     }
+
+    setIsLoading(false)
   }
 
   return (
-    <div className="mt-14">
+    <div className="my-14">
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
@@ -51,19 +60,37 @@ const Search = () => {
       >
         {({ values }) => (
           <Form noValidate>
-            <h1 className='text-center text-yellow-400 text-5xl'>Movie Search</h1>
+            <h1 className='text-center text-yellow-400 text-5xl font-rockSalt'>Movie Search</h1>
 
             <SearchForm setResults={setResults} />
 
-            {results.length > 0 && (
+            {isLoading && (
+              <div className="relative">
+                <Spinner />
+              </div>
+            )}
+
+            {!isLoading && showNoResults && (
+              <div className="text-yellow-400">
+                <h3 className="text-3xl">Hmmm...</h3>
+
+                <p className="text-2xl my-3">We couldn't find any matches for <span className="font-bold italic">"{values.searchTerm}"</span></p>
+
+                <p>Double check your search for any typos or spelling errors - or try a different search term.</p>
+              </div>
+            )}
+
+            {!isLoading && results?.length > 0 && (
               <>
                 <MovieList results={results} />
 
-                <Pagination
-                  className="my-12"
-                  totalPages={totalPages}
-                  onPageChange={(e) => onSubmit(values, e.selected + 1)}
-                />
+                {totalPages > 1 && (
+                  <Pagination
+                    className="my-12"
+                    totalPages={totalPages}
+                    onPageChange={(e) => onSubmit(values, e.selected + 1)}
+                  />
+                )}
               </>
             )}
           </Form>
